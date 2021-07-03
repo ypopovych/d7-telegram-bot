8685975fc92f8ae2e06670567b43377edbf3ab66
Yehor Popovych <popovych.yegor@gmail.com>
2021-07-04 01:14:07 +0300

    added delay to the setTitle method

--- src/utils/title.ts
+++ src/utils/title.ts
@@ -1,11 +1,13 @@
 import { Telegram } from "telegraf"
 import { Storage } from "../types"
+import { delay } from "./delay"
 
 const MODULE = "title"
 const TITLES_KEY = "titles"
 
 export async function setTitle(telegram: Telegram, storage: Storage, chatId: string, userId: number, title: string): Promise<void> {
     await telegram.promoteChatMember(chatId, userId, { can_invite_users: true })
+    await delay(2000) // wait 2s
     await telegram.setChatAdministratorCustomTitle(chatId, userId, title)
     let titles: Record<number, string> = await storage.getConfigValue(chatId, MODULE, TITLES_KEY) ?? {}
     titles[userId] = title
@@ -17,6 +19,7 @@ export async function restoreTitle(telegram: Telegram, storage: Storage, chatId:
     const title = titles[userId]
     if (title) {
         await telegram.promoteChatMember(chatId, userId, { can_invite_users: true })
+        await delay(2000) // wait 2s
         await telegram.setChatAdministratorCustomTitle(chatId, userId, title)
     }
 }
